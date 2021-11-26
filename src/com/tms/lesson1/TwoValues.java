2744ec774599f1ebfb7bdb5be811ebfef7b06876
Max <kpabomakc@yandex.ru>
2021-11-26 22:03:28 +0200

    add classes in lesson1

---
+++ src/com/tms/lesson1/TwoValues.java
@@ -0,0 +1,10 @@
+package com.tms.lesson1;
+
+public class TwoValues {
+    public static void main(String[] args) {
+        //делим одно на другое
+        int x = 10;
+        int y = 20;
+        System.out.println(y/x);
+    }
+}
